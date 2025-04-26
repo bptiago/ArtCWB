1c8bb4f3fcf77183d01fa213a506c0ad940e7179
bptiago <bptiago123@gmail.com>
2025-04-26 10:31:17 -0300

    Template inicial em ViewCode

---
+++ ArtCWBTests/ArtCWBTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCWBTests.swift
+//  ArtCWBTests
+//
+//  Created by Tiago Prestes on 26/04/25.
+//
+
+import Testing
+@testable import ArtCWB
+
+struct ArtCWBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
